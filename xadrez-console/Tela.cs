37a1172bffd47ddbeafed12ecd3faa6f127e96e2
Gabriel Okumura <Gabrielokumura2017.gmail.com>
2021-09-30 15:53:09 -0300

    implementando a logica de xequemate

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -13,11 +13,20 @@ namespace xadrez_console
             Console.WriteLine();
             imprimirPecasCapturadas(partida);
             Console.WriteLine("Turno " + partida.turno);
-            Console.WriteLine("Aguardando jogada: " + partida.jogardorAtual);
-            if (partida.xeque)
+            if (!partida.terminada)
             {
-                Console.WriteLine("XEQUE!!");
+                Console.WriteLine("Aguardando jogada: " + partida.jogardorAtual);
+                if (partida.xeque)
+                {
+                    Console.WriteLine("XEQUE!!");
+                }
             }
+            else
+            {
+                Console.WriteLine("XEQUEMATE!!");
+                Console.WriteLine("vencedor: " + partida.jogardorAtual);
+            }
+            
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
